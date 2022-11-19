dead17ebf0249fb6a21e924124536d72cf2ad517
oswald-pro <oswaldmboussa1@gmail.com>
2022-11-19 09:17:55 +0100

    commit

--- Nested If/Code2.py
+++ Nested If/Code2.py
@@ -5,16 +5,16 @@ products_price = int(input("Enter the cost of the products:"))
 
 if country == "US":
     if products_price in range(0, 100):
-        print("Shipping cost is $25")
+        print("Shipping cost for US is $25")
     elif products_price in range(101, 150):
         print("Shipping cost is $5")
     else:
-        print("Free shipping")
+        print("Free shipping for US")
     
-elif country == 'UK':
+elif country == "UK":
     if products_price in range(0, 50):
-        print('Shipping cost is $50')
+        print('Shipping cost for UK is $50')
     else:
-        print('Free shipping')
+        print('Free shipping for UK')
 else:
     print("Sorry ! we deliver our product only in US and UK")
