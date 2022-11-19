8fc5ab9a0a3e4fc5d746ee7140e84eba5ffb111a
oswald-pro <oswaldmboussa1@gmail.com>
2022-11-19 09:15:29 +0100

    commit

---
+++ Nested If/Code2.py
@@ -0,0 +1,20 @@
+# Nested If with the use of range method
+country = input("Enter your country name:")
+products_price = int(input("Enter the cost of the products:"))
+
+
+if country == "US":
+    if products_price in range(0, 100):
+        print("Shipping cost is $25")
+    elif products_price in range(101, 150):
+        print("Shipping cost is $5")
+    else:
+        print("Free shipping")
+    
+elif country == 'UK':
+    if products_price in range(0, 50):
+        print('Shipping cost is $50')
+    else:
+        print('Free shipping')
+else:
+    print("Sorry ! we deliver our product only in US and UK")
