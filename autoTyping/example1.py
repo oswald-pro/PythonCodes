d281095089ecffa0590a6fe7b8946ac7ddc59a3e
Oswald MBOUSSA <60357182+oswald-pro@users.noreply.github.com>
2022-12-28 17:14:40 +0100

    Create example1.py

---
+++ autoTyping/example1.py
@@ -0,0 +1,10 @@
+# Here is a simple Python script that can be used to automatically type out 
+# a string of text. It uses the pyautogui library to simulate the keystrokes.
+
+import pyautogui
+
+# Type out the string of text
+pyautogui.typewrite('Hello, world!')
+
+# You can also use the press() function to simulate individual key presses
+pyautogui.press('enter')
