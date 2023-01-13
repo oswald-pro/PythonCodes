c7309a89d192c4676fd2671102b9df2bafd781dc
oswald-pro <oswaldmboussa1@gmail.com>
2023-01-13 16:18:59 +0100

    commit

--- autoTyping/example1.py
+++ autoTyping/example1.py
@@ -1,10 +1,18 @@
-# Here is a simple Python script that can be used to automatically type out 
-# a string of text. It uses the pyautogui library to simulate the keystrokes.
+# Here's a simple example of how you can output a string in a "typing" way using Python
 
-import pyautogui
+import time
 
-# Type out the string of text
-pyautogui.typewrite('Hello, world!')
 
-# You can also use the press() function to simulate individual key presses
-pyautogui.press('enter')
+def type_string(string):
+    for character in string:
+        print(character, end='', flush=True)
+        time.sleep(0.05)
+
+
+type_string("""
+This code will prompt the user to enter the name of an image file, read the image file, and 
+then display it in a window with the title 'Image'. The waitKey function will pause the  
+program until a key is pressed, and the destroyAllWindows function will close the window 
+when the program is finished.
+
+I hope this helps! Let me know if you have any questions.""")
